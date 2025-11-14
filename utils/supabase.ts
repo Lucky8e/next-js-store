15be93a44e0b7c0c34ab808ec332101ba3b99cbd
Lucky8e <luckysingh8e24@gmail.com>
2025-11-14 10:16:40 +0530

    Fix Supabase client, RLS policies, and env vars

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,12 +1,24 @@
+// import { createClient } from "@supabase/supabase-js";
+
+// const bucket = "main-bucket";
+// export const supabase = createClient(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+// );
+
 import { createClient } from "@supabase/supabase-js";
 
 const bucket = "main-bucket";
-export const supabase = createClient(
-  process.env.SUPABASE_URL as string,
-  process.env.SUPABASE_Key as string
-);
+
+export function getSupabase() {
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+}
 
 export const uploadImage = async (image: File) => {
+  const supabase = getSupabase();
   const timestamp = Date.now();
   const newName = `${timestamp}-${image.name}`;
   const { data } = await supabase.storage
@@ -17,6 +29,7 @@ export const uploadImage = async (image: File) => {
 };
 
 export const deleteImage = (url: string) => {
+  const supabase = getSupabase();
   const imageName = url.split("/").pop();
   if (!imageName) throw new Error("Invalid Url");
   return supabase.storage.from(bucket).remove([imageName]);
